097ee69af61333258e28e4bd1598290bdb33e430
HolyDinosaur <jsvyatoslav@gmail.com>
2025-02-05 09:57:56 +0300

    Worked on a task Final Сertification v12

--- recipe_site/recipes/forms.py
+++ recipe_site/recipes/forms.py
@@ -1,8 +1,15 @@
 from django import forms
 from .models import Recipe, Category
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class RecipeForm(forms.ModelForm):
     class Meta:
         model = Recipe
-        fields = ['title', 'description', 'preparation_steps', 'cooking_time', 'image', 'categories', 'author']
+        fields = ['title', 'description', 'preparation_steps', 'cooking_time', 'image', 'categories']
 
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
